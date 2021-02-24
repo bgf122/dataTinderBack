92b4df9efcd31911f58d86646b8bbbd6da10809a
Sasu Korhonen <smage90@hotmail.com>
2021-02-24 18:41:45 +0200

    Fetching single Id and a random

--- src/api/components/programs/controller.js
+++ src/api/components/programs/controller.js
@@ -1,12 +1,22 @@
 const Program = require('./model');
 
 exports.get_all_programs = async (req, res) => {
-    try {
-        const programs = await Program.find();
-        res.json(programs);
-    } catch (err) {
-        res.json({ message:err });
+    if (req.query.random !== undefined) {
+        try {
+            const program = await Program.aggregate([{ $sample: { size: Number(req.query.random) } }])
+            res.json(program);
+        } catch (err) {
+            res.json({ message:err });
+        }
+    } else {
+        try {
+            const programs = await Program.find();
+            res.json(programs);
+        } catch (err) {
+            res.json({ message:err });
+        }
     }
+    
 };
 
 exports.get_program = async (req, res) => {
@@ -18,13 +28,3 @@ exports.get_program = async (req, res) => {
     }
 };
 
-exports.get_random_program = async (req, res) => {
-    try {
-        const program = await Program.aggregate([{ $sample: { size: 1 } }])
-            res.json(program); 
-    } catch (err) {
-        res.json({ message:err });
-
-    }
-	
-};
--- src/api/components/programs/routes.js
+++ src/api/components/programs/routes.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 
-// Get all programs
+// Get all programs or a random programs with ?random={number}
 router.get('/', controller.get_all_programs);
 
 // Get Specific program by id
 router.get('/:programId', controller.get_program);
 
-// Get random program
-router.get("/api/programs/x", controller.get_random_program);
+
 
 module.exports = router;
