2de76cf5859cf236b4b9edd9e22d7bb6d826bd8b
jussihayha <45433002+jussihayha@users.noreply.github.com>
2021-02-28 08:50:48 +0200

    Added toJSON transformation to programSchema that removes Mongo default _ID when getting all programs or certain program

--- src/api/components/programs/model.js
+++ src/api/components/programs/model.js
@@ -1,38 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const programSchema = mongoose.Schema(
-    {
-        id: {
-            type: String,
-            required: true,
-        },
-        title: {
-            type: String,
-            required: true,
-        },
-        transmission_title: {
-            type: Object,
-            required: true,
-        },
-        part_of_series_title: {
-            type: String,
-            required: true,
-        },
-        publication_event: {
-            type: Object,
-            required: true,
-        },
-        image_id: {
-            type: Object,
-            required: true,
-        },
-        genres: {
-            type: Object,
-            required: true,
-        },
-    },
-    {
-        collection: "rajapinta",
-    });
+	{
+		id: {
+			type: String,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		transmission_title: {
+			type: Object,
+			required: true,
+		},
+		part_of_series_title: {
+			type: String,
+			required: true,
+		},
+		publication_event: {
+			type: Object,
+			required: true,
+		},
+		image_id: {
+			type: Object,
+			required: true,
+		},
+		genres: {
+			type: Array,
+			required: true,
+		},
+	},
+	{
+		collection: "rajapinta",
+	}
+);
 
-module.exports = mongoose.model('Programs', programSchema);
+programSchema.set("toJSON", {
+	transform: (document, returnedDocument) => {
+		document._id = returnedDocument._id;
+		delete returnedDocument._id;
+		delete returnedDocument.__v;
+	},
+});
+
+/* We have to change this if we want to use multiple Mongo DB's.
+https://mongoosejs.com/docs/connections.html
+*/
+module.exports = mongoose.model("Program", programSchema);
