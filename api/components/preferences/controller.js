5b4eec849ca25b1f4b17e8f7088665bd6297695c
Jussi Häyhä <jussihayha@gmail.com>
2021-04-05 20:44:53 +0300

    Muokattu käyttöön res.locals.uid, laitettu testiin toinen versio tykkäysten tallentamisesta.

--- api/components/preferences/controller.js
+++ api/components/preferences/controller.js
@@ -3,12 +3,13 @@ const admin = require('firebase-admin');
 const db = admin.database();
 
 exports.saveUserPreference = async (req, res) => {
+
 	try {
 		const prefence = { 
             program_id: req.body.program_id,
             swipe: req.body.swipe
         }
-        const savedData = await db.ref('preferences').child(req.body.user_id).push(prefence);
+        const savedData = await db.ref('preferences').child(res.locals.uid).push(prefence);
         res.json(savedData);
 	} catch (err) {
 		res.json({ message: err });
