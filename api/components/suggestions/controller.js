e4e041afc407cf7de7e9e776d5f5e6fc50993966
Jussi Häyhä <jussihayha@gmail.com>
2021-04-10 11:51:40 +0300

    Muutettu, että backend palauttaa ID:n kun hakee suosituksia

--- api/components/suggestions/controller.js
+++ api/components/suggestions/controller.js
@@ -3,8 +3,7 @@ const Program = require("../programs/model");
 exports.getSuggestions = async (req, res) => {
 	try {
 		const suggestions = await Program.aggregate([
-			{ $sample: { size: Number(req.params.amount || 1) } },
-			{ $project: { _id: 0 } },
+			{ $sample: { size: Number(req.params.amount || 1) } }
 		]);
 		res.json(suggestions);
 	} catch (err) {
