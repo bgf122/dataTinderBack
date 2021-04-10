e2d3a18fb9495c30602a9798d0ee269c860fb27f
Jussi Häyhä <jussihayha@gmail.com>
2021-04-10 14:40:58 +0300

    Vaihdettu skeemaksi 'anytype'. Backend ei tallenna tietoja mongoon, joten tämä ok

--- api/components/programs/model.js
+++ api/components/programs/model.js
@@ -1,50 +1,6 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const programSchema = mongoose.Schema(
-	{
-		id: {
-			type: String,
-			required: true,
-		},
-		title: {
-			type: String,
-			required: true,
-		},
-		transmission_title: {
-			type: Object,
-			required: true,
-		},
-		part_of_series_title: {
-			type: String,
-			required: true,
-		},
-		publication_event: {
-			type: Object,
-			required: true,
-		},
-		image_id: {
-			type: Object,
-			required: true,
-		},
-		genres: {
-			type: Array,
-			required: true,
-		},
-	},
-	{
-		collection: "rajapinta",
-	}
-);
+const programSchema = new Schema({ any: Schema.Types.Mixed });
 
-programSchema.set("toJSON", {
-	transform: (document, returnedDocument) => {
-		document._id = returnedDocument._id;
-		delete returnedDocument._id;
-		delete returnedDocument.__v;
-	},
-});
-
-/* We have to change this if we want to use multiple Mongo DB's.
-https://mongoosejs.com/docs/connections.html
-*/
-module.exports = mongoose.model("Program", programSchema);
+module.exports = mongoose.model("Program", programSchema, "rajapinta");
--- api/components/suggestions/controller.js
+++ api/components/suggestions/controller.js
@@ -2,6 +2,7 @@ const Program = require("../programs/model");
 
 exports.getSuggestions = async (req, res) => {
 	try {
+		console.log(req.params.amount);
 		const suggestions = await Program.aggregate([
 			{ $sample: { size: Number(req.params.amount || 1) } }
 		]);
