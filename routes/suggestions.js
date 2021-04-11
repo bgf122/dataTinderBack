0d36fbfb39610e67d33b2be68b449a6754dd2187
Jussi Häyhä <jussihayha@gmail.com>
2021-04-11 06:56:03 +0300

    Korjattu suggestions antamaan myös default palautuksen, jos määrää ei ole annettu

--- routes/suggestions.js
+++ routes/suggestions.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const service = require("../service/suggestions");
 
-router.get("/:amount", service.getSuggestions);
+router.get("/:amount?", service.getSuggestions);
 
 module.exports = router;
