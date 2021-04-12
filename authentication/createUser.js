bbd685cb7f11f49cb60374088002f956e1265ac7
Sasu Korhonen <smage90@hotmail.com>
2021-04-12 09:41:32 +0300

    korjauksia käyttäjän luontiin

--- authentication/createUser.js
+++ authentication/createUser.js
@@ -8,7 +8,7 @@ exports.createUser = async (req, res) => {
                 email: req.body.email,
                 firstName: req.body.firstName,
                 lastName: req.body.lastName,
-                displayName: `${req.body.firstname} + ${req.body.lastname}`,
+                displayName: `${req.body.firstName} + ${req.body.lastName}`,
                 password: req.body.password,
                 disabled: false,
         })
