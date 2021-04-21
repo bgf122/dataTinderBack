ee3e156282a9398b9d634d2f25183cd9b9f7429d
Sasu Korhonen <smage90@hotmail.com>
2021-04-21 18:22:41 +0300

    suggestions

--- service/suggestions.js
+++ service/suggestions.js
@@ -14,17 +14,17 @@ exports.getSuggestions = async (req, res) => {
       const suggestions = await Program.aggregate([
         { $sample: { size: Number(req.params.amount || 1) } },
       ]);
-      return res.json(suggestions.map((suggestion) => ({ ...suggestion, suggestionType: 'random' })));
+      res.json(suggestions.map((suggestion) => ({ ...suggestion, suggestionType: 'random' })));
     }
 
     // käyttäjällä on tämän requestin hetkellä vähintään 5 swaippia ja kokonaismäärä on jaollinen 5:llä.
     // palautetaan KNN recommenderin suosittelema ohjelma.
     const recommendation = await recommendationsService.getKmeansSuggestion({ ...req, body: { id: res.locals.user.uid } }, res);
-    return res.json([{ ...recommendation, suggestionType: 'match' }]);
+    res.json([{ ...recommendation, suggestionType: 'match' }]);
 
   } catch (err) {
     console.log("getSuggestions")
-    return res.json({ error: err.message });
+    res.json({ error: err.message });
   
   }
 };
